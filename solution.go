4eae24f6166172a28f5a09b5aa16a6d9dcbf3737
rom <digam19@gmail.com>
2022-02-18 11:13:59 +0300

    22

--- solution.go
+++ solution.go
@@ -9,6 +9,13 @@ import 	"math"
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
+
+const  (
+	SidesSquare = 4
+	SidesTriangle = 3
+	SidesCircle = 0
+)
+
 func CalcSquare(sideLen float64, sidesNum int8) float64 {
 	switch sidesNum {
 	case 0:
